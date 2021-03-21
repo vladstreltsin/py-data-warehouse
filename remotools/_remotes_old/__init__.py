13ed6d961cece7e37403662d0a5495ac9451f8a5
vladstreltsin@github.com <vladstreltsin@gmail.com>
2021-03-21 12:24:26 -0500

    remove old

--- remotools/_remotes_old/__init__.py
+++
@@ -1,5 +0,0 @@
-from remotools._remotes_old.s3 import S3Remote
-from remotools._remotes_old.gs import GSRemote
-from remotools._remotes_old.uri import URIRemote, CachingURIRemote
-from remotools._remotes_old.hfs import HFSRemote
-from remotools._remotes_old.local import LocalRemote
